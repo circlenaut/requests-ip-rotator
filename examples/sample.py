99ec5c418a4a4f5904aa6a79b5f3a0a01f44ab4f
Phillip Strefling <phillip@strefling.pro>
2021-09-03 15:54:38 -0400

    added credentials loading

--- examples/sample.py
+++ examples/sample.py
@@ -1,17 +1,55 @@
-import requests
-from requests_ip_rotator import ApiGateway
+import json as _json
+import logging as _logging
+import pathlib as _pathlib
+import requests as _requests
 
-# Create gateway object and initialise in AWS
-gateway = ApiGateway("https://site.com")
-gateway.start()
+from requests_ip_rotator import ApiGateway as _ApiGateway
 
-# Assign gateway to session
-session = requests.Session()
-session.mount("https://site.com", gateway)
 
-# Send request (IP will be randomised)
-response = session.get("https://site.com/index.html")
-print(response.status_code)
+CREDENTIALS_FILE = './credentials.json'
+TEST_SITE = 'http://www.google.com'
+EXTRA_REGIONS = ["us-east-1", "us-east-2"]
+LOG_LEVEL = "info"
 
-# Delete gateways
-gateway.shutdown()
+def main() -> None:
+    
+    ### Load credentials 
+    credentials_path = _pathlib.Path(CREDENTIALS_FILE)
+    if not credentials_path.exists():
+        _log.error(f"invalid credentials path: '{credentials}'")
+    
+    with open(credentials_path, 'r') as json_file:
+        credentials = _json.load(json_file)
+    
+    # Create gateway object and initialise in AWS
+    gateway = _ApiGateway(
+        site = TEST_SITE,
+        regions=EXTRA_REGIONS,
+        access_key_id = credentials.get('access_key_id'),
+        access_key_secret = credentials.get('access_key_secret'),
+    )
+    gateway.start()
+
+    # Assign gateway to session
+    session = _requests.Session()
+    session.mount(TEST_SITE, gateway)
+
+    # Send request (IP will be randomised)
+    response = session.get(f"{TEST_SITE}/index.html")
+    _log.info(f"status code: '{response.status_code}'")
+
+    # Delete gateways
+    gateway.shutdown()
+
+if __name__ == '__main__':
+    
+    ### Enable Logging
+    _logging.basicConfig(
+        format = '[%(levelname)s] %(message)s',
+        datefmt = '%Y-%m-%d %H:%M:%S %z',
+        level = LOG_LEVEL.upper(),
+    )
+    _log = _logging.getLogger(__name__)
+
+    ### Run main
+    main()
