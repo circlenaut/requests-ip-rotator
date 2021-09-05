ae356d4ca92382f0c2d9f2b5247660fdb3374746
Phillip Strefling <phillip@strefling.pro>
2021-09-05 12:40:30 -0400

    restructured project

--- examples/sample.py
+++ examples/sample.py
@@ -30,7 +30,7 @@ def main() -> None:
         log_level = LOG_LEVEL.upper()
     )
     gateway.start()
-    _log.info(f"status: {gateway.status()}")
+    gateway.status()
 
     # Assign gateway to session
     session = _requests.Session()
