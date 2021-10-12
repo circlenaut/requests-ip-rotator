e4acf97b4d1e0b958a87bcac3dd620346b81a368
Phillip Strefling <phillip@strefling.pro>
2021-10-12 17:54:13 -0400

    added reset and status util

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -33,7 +33,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - `setup.py`
 
 
-## [1.0.9] - 2020-09-03
+## [1.0.9] - 2021-09-03
 ### Added
 
 - Forked repo: https://github.com/Ge0rg3/requests-ip-rotator
---
+++ status.py
@@ -0,0 +1,46 @@
+import json as _json
+import logging as _logging
+import pathlib as _pathlib
+import requests as _requests
+
+from requests_ip_rotator import ApiGateway as _ApiGateway
+
+
+CREDENTIALS_FILE = './examples/credentials.json'
+TEST_SITE = 'http://www.google.com'
+EXTRA_REGIONS = ["us-east-1", "us-east-2"]
+LOG_LEVEL = "debug"
+
+def main() -> None:
+    
+    ### Load credentials 
+    credentials_path = _pathlib.Path(CREDENTIALS_FILE)
+    if not credentials_path.exists():
+        _log.error(f"invalid credentials path: '{credentials}'")
+    
+    with open(credentials_path, 'r') as json_file:
+        credentials = _json.load(json_file)
+    
+    # Create gateway object and initialise in AWS
+    gateway = _ApiGateway(
+        site = TEST_SITE,
+        regions=EXTRA_REGIONS,
+        access_key_id = credentials.get('access_key_id'),
+        access_key_secret = credentials.get('access_key_secret'),
+        log_level = LOG_LEVEL.upper()
+    )
+    # Get status
+    gateway.status()
+
+if __name__ == '__main__':
+    
+    ### Enable Logging
+    _logging.basicConfig(
+        format = '[%(levelname)s] %(message)s',
+        datefmt = '%Y-%m-%d %H:%M:%S %z',
+    )
+    _log = _logging.getLogger(__name__)
+    _log.setLevel(LOG_LEVEL.upper())
+
+    ### Run main
+    main()
